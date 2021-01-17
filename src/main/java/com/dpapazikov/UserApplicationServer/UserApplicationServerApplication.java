79136006e1c74fc541be2279f82f7f559678e161
gitDimitar <dpapazikov@gmail.com>
2021-01-17 21:42:11 +0000

    innit

---
+++ src/main/java/com/dpapazikov/UserApplicationServer/UserApplicationServerApplication.java
@@ -0,0 +1,13 @@
+package com.dpapazikov.UserApplicationServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApplicationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserApplicationServerApplication.class, args);
+	}
+
+}
